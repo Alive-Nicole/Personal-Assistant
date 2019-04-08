42041dc3d32005659a96ac86c9db46f6fdcce6d0
Alive-Nicole <nicole.nwakalor@gmail.com>
2019-04-08 00:32:14 -0400

    main component

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,11 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { WeekDay } from '@angular/common';
+// import {NgbCheckBox} from '@ng-bootstrap/ng-bootstrap'
 
 @Component({
-  selector: 'app-main',
+  selector: 'main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  tasks = [{
+    id: 0,
+    task: "Go for the meeting",
+    completed: false
+  },{
+    id: 1,
+    task: "Do the laundary",
+    completed: false
+  },{
+    id: 2,
+    task: "Wash the dishes",
+    completed: false
+  }];
+
+  formateDate(scope){
+    scope.today = new Date();
+  }
+  
+
 
   constructor() { }
 
